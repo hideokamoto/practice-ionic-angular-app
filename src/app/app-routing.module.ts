6441defdde7165feb48d604a4f2326eb07ac775b
Hidetaka Okamoto <kokkoku214@gmail.com>
2022-07-23 01:34:36 +0900

    init tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    children: [{
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+      },
+      {
+        path: 'souvenir',
+        loadChildren: () => import('./pages/souvenir/souvenir.module').then( m => m.SouvenirPageModule)
+      },{
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      }]
   },
   {
     path: 'message/:id',
     loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'souvenir',
-    loadChildren: () => import('./pages/souvenir/souvenir.module').then( m => m.SouvenirPageModule)
   },
 ];
 
